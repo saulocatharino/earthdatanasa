e18d51ce556b66e82ddd47742e327d15f2cece83
Saulo Catharino <saulocatharino@gmail.com>
2021-10-12 19:38:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,11 @@ import numpy as np
 from datetime import date, timedelta
 import time
 import streamlit as st
-
+from utils import *
 
 side = st.sidebar
 
 
-
-# Função para converter a posição de Latitude e Longitude para a escala
-def convert(lat,lon,level):
-	row = ((90 - lat) * (2 ** level)) // 288
-	col = ((180 + lon) * (2 ** level)) // 288
-	return int(row),int(col)
 
 
 # Seleciona a banda para aquisição de imagem
